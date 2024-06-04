8792725bceb09ef7c7961a2a1cc857cbc0d06d1d
budicuy <personal.budicuy@gmail.com>
2024-06-04 13:55:31 +0800

    Membuat Fungsi Dmenu()

--- P-10/main.cpp
+++ P-10/main.cpp
@@ -4,5 +4,17 @@ using namespace std;
 int main()
 {
 
-    cout << "Halo Dunia" << endl;
+    cout << "Halo Dunia" << fendl;
+}
+
+void dMenu()
+{
+    system("cls");
+    cout << "Aplikasi Template Menu" << endl;
+    cout << "1. Menu Pertama" << endl;
+    cout << "2. Menu Kedua" << endl;
+    cout << "3. Menu Ketiga" << endl;
+    cout << "4. Menu Keempat" << endl;
+    cout << "5. Exit" << endl;
+    cout << "Masukan Angka : ";
 }
