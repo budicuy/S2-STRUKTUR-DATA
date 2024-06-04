3aec6dc5fc6e3bb045495a831c593101d145df84
budicuy <personal.budicuy@gmail.com>
2024-06-04 13:59:46 +0800

    buat menu berulang ketika mengetikan kecuali selain 5 di keyboard

--- P-10/main.cpp
+++ P-10/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 int main()
 {
 
-    cout << "Halo Dunia" << fendl;
+    char pl;
+    do
+    {
+        dMenu();
+        pl = getch();
+    } while (pl != '5');
 }
 
 void dMenu()
