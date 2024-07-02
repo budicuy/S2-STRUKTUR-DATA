031b10e89d5f57f505935fdc7672673644aad035
budicuy <personal.budicuy@gmail.com>
2024-07-02 11:56:06 +0800

    update

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "i'm learnning on poliban, and i'm progammer!" << endl;
+    return 0;
+}
